8933ef5149bff60f31e3d5a080450f18d7b321e3
Jorge Mina <minajorge@hotmail.com>
2020-11-07 08:25:26 -0500

    Initial commit

---
+++ src/main/java/com/autentication/java/models/ERole.java
@@ -0,0 +1,7 @@
+package com.autentication.java.models;
+
+public enum ERole {
+	ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/autentication/java/models/Role.java
@@ -0,0 +1,22 @@
+package com.autentication.java.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+}
